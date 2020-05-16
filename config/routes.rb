39e74643d1f206a1ef34bb19fab266ac7439b4f5
Jan Lesko <jan.lesko.sk@gmail.com>
2020-05-16 13:45:31 +0000

    Add the About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'home', to: 'static_pages#home'
-  get 'help', to: 'static_pages#help'
+  get 'home',  to: 'static_pages#home'
+  get 'help',  to: 'static_pages#help'
+  get 'about', to: 'static_pages#about'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get about" do
+    get about_url
+    assert_response :success
+  end
 end
