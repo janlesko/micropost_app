0935fe0bc89578dbb780c5a26f125ebd80b77f8c
Jan Lesko <jan.lesko.sk@gmail.com>
2020-05-16 14:14:32 +0000

    Add dynamic titles to the Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Micropost App"
+  end
+  
   test "should get home" do
     get home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 end
