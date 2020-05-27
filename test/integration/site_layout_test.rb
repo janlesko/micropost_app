94ff9857c952ba34dac6319f5bef5d954e6ce8cb
Jan Lesko <jan.lesko.sk@gmail.com>
2020-05-27 22:56:48 +0000

    Add the users index

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,13 +1,24 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+  end
 
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    log_in_as(@user)
+    follow_redirect!
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path
+    assert_select "a[href=?]", edit_user_path
+    assert_select "a[href=?]", logout_path
   end
 end
