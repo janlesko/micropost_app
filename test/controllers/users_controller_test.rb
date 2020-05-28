4922eddd813db4ba5c61a0001f5eca33edb0cfbb
Jan Lesko <jan.lesko.sk@gmail.com>
2020-05-28 11:08:25 +0000

    Add the Admin role to Users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user       = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "should get new" do
     get signup_url
     assert_response :success
@@ -9,5 +15,28 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should redirect index when not logged in" do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              "password",
+                                            password_confirmation: "password",
+                                            admin: true } }
+    assert_not @other_user.reload.admin?
   end
 end
