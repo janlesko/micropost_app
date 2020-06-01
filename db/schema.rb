6790323a7a084e522a61e1a541d6e9cf9e84ae6c
Jan Lesko <jan.lesko.sk@gmail.com>
2020-06-01 11:36:01 +0000

    Add the Micropost model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_31_113713) do
+ActiveRecord::Schema.define(version: 2020_06_01_104106) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_05_31_113713) do
     t.datetime "reset_sent_at"
   end
 
+  add_foreign_key "microposts", "users"
 end
