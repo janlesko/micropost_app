ccf16b8aa6da624a9cb400df9d41397bdd279018
Jan Lesko <jan.lesko.sk@gmail.com>
2020-06-05 15:01:17 +0000

    Add image uploads to Microposts

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Invalid submission
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
@@ -18,9 +19,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', '/?page=2'  # Correct pagination link
     # Valid submission
     content = "This micropost really ties the room together"
+    image = fixture_file_upload('test/fixtures/files/pg.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost:
+                                      { content: content, image: image } }
     end
+    assert assigns(:micropost).image.attached?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
