ef19cd4b4fb19f9a1cf46f1cd97be50446f17837
Jan Lesko <jan.lesko.sk@gmail.com>
2020-06-10 08:00:09 +0000

    Refactor the User's feed method

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,10 +85,8 @@ class User < ApplicationRecord
 
   # Returns a user's status feed
   def feed
-    following_ids = "SELECT followed_id FROM relationships
-                     WHERE follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids})
-                     OR user_id = :user_id", user_id: id)
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    Micropost.joins(user: :followers).where(part_of_feed, { id: id })
   end
 
   # Follows a user.
